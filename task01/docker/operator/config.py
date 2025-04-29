fe406be7b8aa24f40e1b27e2cbaca5e362407f06
Michal Smyczynski <michal.smyczynski@intive.com>
2025-04-29 21:28:13 +0200

    make ca cert path as option

--- task01/docker/operator/config.py
+++ task01/docker/operator/config.py
@@ -3,10 +3,9 @@ import os
 def get_kafka_config():
     config = {
         'bootstrap_servers': os.environ.get('KAFKA_BOOTSTRAP', 'localhost:9094'),
-        'security_protocol': os.environ.get('KAFKA_SECURITY_PROTOCOL', 'SASL_SSL'),
-        'ssl_cafile': os.environ.get('KAFKA_CA_CERT', '/certs/ca.crt'),
-        'sasl_mechanism': os.environ.get('KAFKA_SASL_MECHANISM', 'SCRAM-SHA-512'),
-        'sasl_plain_username': os.environ.get('KAFKA_SASL_USERNAME', ''),
-        'sasl_plain_password': os.environ.get('KAFKA_SASL_PASSWORD', '')
+        'security_protocol': "SSL",
+        **({'ssl_cafile': os.environ["KAFKA_CA_CERT"]} if "KAFKA_CA_CERT" in os.environ else {}),
+        'ssl_certfile': os.environ.get("KAFKA_CLIENT_CERT", "/certs/client.crt"),
+        'ssl_keyfile': os.environ.get("KAFKA_CLIENT_KEY", "/certs/client.key")
     }
     return config
